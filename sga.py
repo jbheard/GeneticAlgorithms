01db19dd2e9fba9814f8872387065bea995d35fe
Jacob Heard <heard.jacob.7@gmail.com>
2018-12-03 00:14:00 -0500

    Removed decode/encode, reworked SGA functions, etc.

--- sga.py
+++ sga.py
@@ -9,207 +9,105 @@ import matplotlib.pyplot as plt
 from copy import deepcopy
 from objfunc import *
 
-# Min and max for floating point numbers
-MINRNG, MAXRNG = -5.12, 5.12
-NUM_TERMS = 3 # How many 4 bit terms to use for decoding
-PRECISION = NUM_TERMS-1 # How many decimal places to add for floating point decoding
-BCD = 4 # length of one digit in bits for BCD 
-
 # Get a True value with certain probability, otherwise False
 # 0 <= probability <= 1
 def flip(probability):
 	return random.random() < probability
 
+# Flip a random bit for a given digit
+def flip_bit_bcd(x):
+	x = int(x * (10**PRECISION)) # Convert to integer (keeping precision)
+	sign = -1 if x < 0 else 1 # Get sign of number
+	
+	# Give sign bit a 1/4 chance compared to full digits (BCD)
+	k = random.randint(-1, (NUM_DIGITS-1) * 4)
+
+	# Flip sign bit and return
+	if k < 0: return -x
+	else: k //= 4
+	
+	x = abs(x)
+	digit = (x // (10**k)) % 10 # Get random digit k
+	x -= digit * (10**k)        # Clear digit k
+
+	# Select only values which will result in a valid output
+	exclusions = [ j for j in range(0, 9+1) if not (MINRNG <= (x + j*(10**k))//(10**PRECISION) <= MAXRNG) ]
+
+	# Get options for flipping 1 bit based on digit
+	if   digit == 0: opts = [1, 2, 4, 8] # 0000 -> 1000, 0100, 0010, 0001
+	elif digit == 1: opts = [0, 3, 5, 9] # 1000 -> 0000, 1100, 1010, 1001
+	elif digit == 2: opts = [0, 3, 6]    # 0100 -> 1100, 0000, 0110
+	elif digit == 3: opts = [1, 2, 7]    # 1100 -> 0100, 1000, 1110
+	elif digit == 4: opts = [0, 5, 6]    # 0010 -> 1010, 0110, 0000
+	elif digit == 5: opts = [1, 4, 7]    # 1010 -> 0010, 1110, 1000
+	elif digit == 6: opts = [2, 4, 7]    # 0110 -> 1110, 0010, 0100
+	elif digit == 7: opts = [3, 5, 6]    # 1110 -> 0110, 1010, 1100
+	elif digit == 8: opts = [0, 9]       # 0001 -> 1001, 0000
+	elif digit == 9: opts = [1, 8]       # 1001 -> 0001, 1000
+
+	# Remove any invalid digits
+	opts = [x for x in opts if x not in exclusions]
+	if len(opts) == 0: return x
+	
+	# Set digit k to new digit
+	ret = x + random.choice( opts ) * (10**k)
+	ret = sign * (ret / (10**PRECISION))
+	return ret
+
+def mutate(gene, pmutate, flip_bit):
+	updated = False
+	if flip(pmutate):
+		chrom = gene.chrom
+		i = random.randint(0, len(chrom)-1)
+		
+		chrom[i] = flip_bit(chrom[i])
+
+		# Update chromosome and set updated to true
+		gene.set_chrom(chrom)
+		updated = True
+
+	return updated
+
+def crossover(parent1, parent2, pcross):
+	child1, child2 = deepcopy(parent1), deepcopy(parent2) # Set children
+
+	if flip(pcross) and len(child1.chrom) > 1:
+		chrom1, chrom2 = child1.chrom, child2.chrom
+		i = random.randint(1, len(chrom1)-1)
+		chrom1[:i], chrom2[i:] = chrom2[:i], chrom1[i:]
+
+		# Update chromosomes
+		child1.set_chrom(chrom1)
+		child2.set_chrom(chrom2)
+
+	return child1, child2
+
+
 # Container class for chromosomes
 class Genotype:
-	def __init__(self, lchrom, fitness=0):
+	def __init__(self, lchrom, generate):
 		# Create list of random boolean values
-		self.chrom = [ flip(0.5) for _ in range(lchrom) ]
-		# Assign fitness value
-		self.fitness = fitness
+		self.chrom = [ generate() for _ in range(lchrom) ]
 		return
 
 	# For printing out gene values
 	def __str__(self):
 		ret = '{:.2f} | '.format(self.fitness)
 		for x in self.chrom:
-			ret += '1' if x else '0'
+			if type(x) == float:
+				ret += ('{:.' + str(PRECISION) + 'f} ').format(x)
+			else:
+				ret += '{} '.format(x)
 		return ret
 
 	# evaluate and update fitness
-	def eval_fitness(self, obj_func, decode_func):
-		self.fitness = obj_func( decode_func(self.chrom) )
+	def eval_fitness(self, obj_func):
+		self.fitness = obj_func( self.chrom )
 		return self.fitness
 
 	def set_chrom(self, newchrom):
 		self.chrom = newchrom
 		return
-
-
-###################### Decoding Functions ########################
-
-# Decode a chromosome (in BCD format + sign bit) into signed floating point numbers with given precision
-def signed_float_decode(chrom):
-	ret = signed_decode(chrom)
-	# Get 2 decimal places (divide by 100) for each decoded signed values
-	ret = [ x/(10.0**PRECISION) for x in ret ]
-	return ret
-
-# Convert a 1-digit decimal number to BCD (list of boolean)
-def dec_to_BCD(x):
-	T, F = True, False
-	if x == 0: return [F, F, F, F]
-	if x == 1: return [T, F, F, F]
-	if x == 2: return [F, T, F, F]
-	if x == 3: return [T, T, F, F]
-	if x == 4: return [F, F, T, F]
-	if x == 5: return [T, F, T, F]
-	if x == 6: return [F, T, T, F]
-	if x == 7: return [T, T, T, F]
-	if x == 8: return [F, F, F, T]
-	if x == 9: return [T, F, F, T]
-	raise ValueError("Invalid value entered")
-
-# Encode a list of floats into a chromosome (in BCD format + sign bit)
-def signed_float_encode(vals):
-	vals = [int(x*(10.0**PRECISION)) for x in vals]
-	return signed_encode(vals)
-
-# Encode a list of signed integers into a chromosome (in BCD format + sign bit)
-def signed_encode(vals):
-	chrom = []
-	for x in vals:
-		if x < 0: x, num = -x, [True]
-		else: num = [False]
-		
-		# Encode the float as boolean values
-		for pwr in range(NUM_TERMS-1, -1, -1):
-			y = x // (10**pwr) % 10
-			num += dec_to_BCD(y)
-		chrom += num
-	return chrom
-
-# Encode a list of unsigned integers into a chromosome (in BCD format + sign bit)
-def unsigned_encode(vals):
-	chrom = []
-	for x in vals:
-		if x < 0: x = -x	
-		num = []
-		
-		# Encode the float as boolean values
-		for pwr in range(NUM_TERMS-1, -1, -1):
-			y = x // (10**pwr) % 10
-			num += dec_to_BCD(y)
-		chrom += num
-	return chrom
-
-# Encode a list of 0/1 values into a chromosome (list of boolean)
-def binary_encode(vals):
-	chrom = [ (x == 1) for x in vals ]
-	return chrom
-
-# Encode a list of +/-1 values into a chromosome (list of boolean)
-def plus_minus_encode(vals):
-	chrom = [ (x == 1) for x in vals ]
-	return chrom
-
-
-# Decode a chromosome (in BCD format) into unsigned floating point numbers with given precision
-def unsigned_float_decode(chrom):
-	ret = unsigned_decode(chrom)
-	# Get 2 decimal places (divide by 100) for each decoded signed values
-	ret = [ x/(10.0**PRECISION) for x in ret ]
-	return ret
-
-# Decode a chromosome (in BCD format + sign bit) into a list of signed integers
-def signed_decode(chrom):
-	ret = []
-	for i in range(0, len(chrom), NUM_TERMS*BCD + 1):
-		num = 0
-		for j in range(i+1, i+NUM_TERMS*BCD, BCD):
-			for k in range(j, j+BCD):
-				num += 2**(k-j) if chrom[k] else 0
-			num *= 10
-		num = num//10 # undo the extra multiply
-		if chrom[i]: num = -num # Set negative bit
-		ret.append( num ) # Add the num to the list
-	return ret
-
-# Decode a chromosome (in BCD format) into a list of unsigned integers
-def unsigned_decode(chrom):
-	ret = []
-	for i in range(0, len(chrom), NUM_TERMS*BCD):
-		num = 0
-		for j in range(i, i+NUM_TERMS*BCD, BCD):
-			for k in range(j, j+BCD):
-				num += 2**(k-j) if chrom[k] else 0
-			num *= 10
-		num = num//10 # undo the extra multiply
-		ret.append( num ) # Add the num to the list
-	return ret
-
-# Decode a chromosome into 0/1 values
-def binary_decode(chrom):
-	ret = []
-	for x in chrom:
-		ret.append(1 if x else 0)
-	return ret
-
-# Decode a chromosome into +/- 1 values
-def plus_minus_decode(chrom):
-	ret = []
-	for x in chrom:
-		ret.append(1 if x else -1)
-	return ret
-
-##################################################################
-
-#################### SGA Operation Functions #####################
-
-# Mutation function, very basic and only changes 1 value in chroma
-def mutate(parent, pmutate, decode_func):
-	childgene = parent.chrom[:]
-
-	if flip(pmutate): # Randomly decide whether to mutate
-		bits = list(range(0, len(childgene)))
-		i = random.choice(bits)
-		bits.remove(i)
-		childgene[i] = not childgene[i] # Flip a random gene
-		while any( [ x > MAXRNG or x < MINRNG for x in decode_func( childgene ) ] ):
-			childgene[i] = not childgene[i]
-			#if len(bits) == 0: break # If this line is needed, something else is wrong
-			i = random.choice(bits)
-			bits.remove(i)
-			childgene[i] = not childgene[i] # Flip a random gene
-		
-
-	child = Genotype(len(parent.chrom))
-	child.set_chrom(childgene)
-	return child
-
-# Crossover of two genes
-def crossover(parent1, parent2, pcross, decode_func, encode_func):
-	chrom1, chrom2 = parent1.chrom[:], parent2.chrom[:]
-
-	# Cross the two genes
-	if flip(pcross):
-		x = decode_func(chrom1)
-		y = decode_func(chrom2)
-		if len(x) > 1:
-			i = random.randint(1, len(x)-1)
-			x[:i], y[i:] = y[:i], x[i:]
-			chrom1, chrom12 = encode_func(x), encode_func(y)
-
-	child1, child2 = Genotype(len(chrom1)), Genotype(len(chrom2))
-	child1.set_chrom(chrom1)
-	child2.set_chrom(chrom2)
-
-	return child1, child2
-
-# Select() is also technically an operator, but is included as part of the SGA class for ease
-
-##################################################################
-
 
 class SGA:
 	'''
@@ -220,38 +118,23 @@ class SGA:
 	obj_func - objective function to apply to chromosomes
 	minimize - if True, attempt to minimize the solution's obj_func, if False, maximize
 	'''
-	def __init__(self, popsize, lchrom, pm, pc, obj_func, decode_func, encode_func, minimize=True):
+	def __init__(self, popsize, lchrom, pm, pc, obj_func, generate, flip_bit, minimize=True):
 		self.pc = pc
 		self.pm = pm
 		self.lchrom = lchrom
 		self.popsize = popsize
 		self.obj_func = obj_func
-		self.decode_func = decode_func
-		self.encode_func = encode_func
+		self.flip_bit = flip_bit
 		self.minimize = minimize
 		
 		# sum and average of fitness for current population
 		self.sumfit = 0
 		
 		# Generate the initial population
-		self.pop = [ Genotype(lchrom) for _ in range(self.popsize) ]
+		self.pop = [ Genotype(lchrom, generate) for _ in range(self.popsize) ]
 		for gene in self.pop:
-			gene.eval_fitness(obj_func, decode_func)
+			gene.eval_fitness(obj_func)
 			
-		# Select arbitrary best gene and update stats to start
-		self.best = self.pop[0]
-		self.update_statistics()
-		return
-
-	# Make the call to this to customize initial population values
-	def init_population(self, rand_func, decode_len):
-		# Generate the initial population
-		self.pop = [ Genotype(lchrom) for _ in range(self.popsize) ]
-		for gene in self.pop:
-			vals = [ rand_func(MINRNG, MAXRNG) for _ in range(decode_len) ]
-			gene.set_chrom( self.encode_func(vals) )
-			gene.eval_fitness(self.obj_func, self.decode_func)
-		
 		# Select arbitrary best gene and update stats to start
 		self.best = self.pop[0]
 		self.update_statistics()
@@ -270,7 +153,7 @@ class SGA:
 			target -= fitness
 			i += 1
 		return i
-	
+
 	# Evolve the current generation
 	def next_generation(self):
 		for _ in range(0, self.popsize, 2): # Do this operation self.popsize/2 times
@@ -279,28 +162,27 @@ class SGA:
 			gene2 = self.select()
 			
 			# Cossover our two selected genes
-			child1, child2 = crossover(self.pop[gene1], self.pop[gene2], self.pc, self.decode_func, self.encode_func)
+			child1, child2 = crossover(self.pop[gene1], self.pop[gene2], self.pc)
 			
-			child1.eval_fitness(self.obj_func, self.decode_func)
-			child2.eval_fitness(self.obj_func, self.decode_func)
-			self.pop[gene1], self.pop[gene2] = child1, child2
-			
+			child1.eval_fitness(self.obj_func)
+			child2.eval_fitness(self.obj_func)
+
+			# Only take children with better fitness than parents
 			if child1.fitness > self.pop[gene1].fitness:
 				self.pop[gene1] = child1
 			if child2.fitness > self.pop[gene2].fitness:
 				self.pop[gene2] = child2
-			#print("Old{} vs New{}".format(self.pop[gene1],child1))
-			#self.pop[gene1], self.pop[gene2] = child1, child2
 		
 		# Mutate all genes with probability pm
 		for i in range(self.popsize):
 			# Mutate the gene
-			self.pop[i] = mutate(self.pop[i], self.pm, self.decode_func)
-			self.pop[i].eval_fitness(self.obj_func, self.decode_func)
+			if mutate(self.pop[i], self.pm, self.flip_bit):
+				self.pop[i].eval_fitness(self.obj_func)
+
 		# Sort the genes in increasing order of fitness
 		self.pop.sort(key=lambda g: g.fitness)
 		return
-	
+
 	# Update information on the current generation
 	def update_statistics(self):
 		# Find total fitness, best gene for this generation
@@ -308,11 +190,11 @@ class SGA:
 		if self.minimize: # Minimize best gene
 			gen_best = min(self.pop, key=lambda x: x.fitness) # Get best gene of generation
 			if gen_best.fitness < self.best.fitness:
-				self.best = gen_best
+				self.best = deepcopy(gen_best)
 		else: # Maximize best gene
 			gen_best = max(self.pop, key=lambda x: x.fitness) # Get best gene of generation
 			if gen_best.fitness > self.best.fitness:
-				self.best = gen_best
+				self.best = deepcopy(gen_best)
 		return
 
 if __name__ == '__main__':
@@ -324,20 +206,20 @@ if __name__ == '__main__':
 	minimize  =       input("Minimize? (Y/N) ------------- > ") # if user answers No, maximize instead
 	minimize  = (minimize[0].lower() == 'y')
 
-	'''
-	NUM_FLOATS = 2
-	lchrom = (NUM_TERMS*BCD+1)*NUM_FLOATS
-	sga = SGA(popsize, lchrom, pmutation, pcross, himmelblau, signed_float_decode, signed_float_encode, minimize)
-	sga.init_population(random.uniform, NUM_FLOATS) # init population
-	'''
+	# GLOBAL VARIABLES
+	MINRNG, MAXRNG = -5.00, 5.00 # Min and max for floating point numbers
+	NUM_DIGITS = 3 # How many significant digits for each value
+	PRECISION = NUM_DIGITS-1 # How many decimal places for floating points (must be <= NUM_DIGITS)
 
-	lchrom = 10 # for objfuncXX, set lchrom = XX
-	sga = SGA(popsize, lchrom, pmutation, pcross, objfunc10, plus_minus_decode, plus_minus_encode, minimize)
-	#sga = SGA(popsize, lchrom, pmutation, pcross, objfunc27, plus_minus_decode, plus_minus_encode, minimize)
-	
-	#lchrom = (NUM_TERMS*BCD+1)*X # Where X is the length of the floating point array to generate
-	#sga = SGA(popsize, lchrom, pmutation, pcross, dejong, signed_float_decode, signed_float)_encode, minimize)
-	#sga = SGA(popsize, lchrom, pmutation, pcross, rosenbrock, signed_float_encode, minimize)
+	lchrom = 27 # for objfuncXX, set lchrom = XX
+	objfunc = objfunc27
+	generate = lambda: random.choice( [-1, 1] ) # lambda for selecting random values
+	sga = SGA(popsize, lchrom, pmutation, pcross, objfunc, generate, lambda x: -x, minimize)
+
+#	lchrom = 2
+#	objfunc = dejong
+#	generate = lambda: random.uniform( MINRNG, MAXRNG )
+#	sga = SGA(popsize, lchrom, pmutation, pcross, objfunc, generate, flip_bit_bcd, minimize)
 
 	# The main body of the SGA, evolve to next generation and update statistics until max no. of generations have been parsed
 	y = []
@@ -346,12 +228,13 @@ if __name__ == '__main__':
 		sga.next_generation()
 		sga.update_statistics()
 		
-		# Add best and average fitness to y vals
+		# TODO: Add min, max, avg fitness to y vals
 		y.append(sga.best.fitness)
 	
-	print("Best Gene:", str(sga.best) )
+	print("Best Gene:", str(sga.best))
 	
 	plt.plot(x, y, label='best')
 	plt.xlabel('Generation #')
 	plt.ylabel('Best, Avg Fitness')
 	plt.show()
+
